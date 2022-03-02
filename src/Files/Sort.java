d8dda654cb1aa71f95fafbb78e9c88b7923f0dd9
Diego <diegoguatedb2002@gmail.com>
2022-03-02 09:43:12 -0600

    Radix sort terminado

--- src/Files/Sort.java
+++ src/Files/Sort.java
@@ -1,9 +1,65 @@
 package Files;
 
+import java.util.Arrays;
+
 public class Sort <T> {
 	public IComparator comparador;
 	
 	public Sort(IComparator comparador) {
 		this.comparador = comparador;
+	}
+	
+	private int getMaxNumber(Integer[] elements) {
+		int max = elements[0];
+		for(int number:elements) {
+			if(number > max) max = number;
+		}
+		return max;
+	}
+	
+	private Integer[] radix_orderByDigit(Integer[] elements, int exp) {
+		
+		Integer[][] numbersByDigit = new Integer[10][elements.length];
+		Integer[] count = new Integer[10];
+		Integer[] output = new Integer[elements.length];
+		
+		Arrays.fill(count, 0); //rellenar count con O's
+		
+		for(Integer number: elements) {
+			
+			int digit = (number / exp) % 10;
+			
+			//agrupar por digito
+			numbersByDigit[digit][count[digit]] = number;			
+			//aumentar la cantidad de numeros con n digito
+			count[digit]++;				
+		}
+		
+		int outputCount = 0;
+		//crear array de salida
+		for(int i = 0; i < numbersByDigit.length; i++) {
+			for(int j = 0; j < numbersByDigit[i].length; j++) {
+				
+				Integer number = numbersByDigit[i][j];				
+				if(number != null) {
+					output[outputCount] = number;
+					outputCount++;
+				}else break;				
+			}
+		}
+		return output;
+	}
+	
+
+	public Integer[] radixSort(Integer[] elements) {
+		
+		if(elements == null || elements.length == 0) return null;
+		int max = getMaxNumber(elements);
+		
+		//aumentar potencia de 10
+		for(int exp = 1; max / exp > 0; exp *= 10) {
+			elements = radix_orderByDigit(elements, exp);
+		}
+		return elements;
 	}
 }
--- src/Tests/SortTests.java
+++ src/Tests/SortTests.java
@@ -9,11 +9,33 @@ import Files.CompareInt;
 import Files.Sort;
 
 class SortTests {
+	
+	private Sort sort;
 
+	public SortTests() {
+		sort = new Sort(new CompareInt());
+	}
+	
 	@Test
-	void test() {
-		Sort prueba = new Sort(new CompareInt());
-		fail("Not yet implemented");
+	void testRadixSort() {
+		
+		Integer[] values1 = {5,47,21,14,59,65,0};
+		var sortedValues1 = sort.radixSort(values1);
+		
+		assertEquals(0, sortedValues1[0]);
+		assertEquals(5, sortedValues1[1]);
+		assertEquals(14, sortedValues1[2]);
+		assertEquals(21, sortedValues1[3]);
+		assertEquals(47, sortedValues1[4]);
+		assertEquals(59, sortedValues1[5]);
+		assertEquals(65, sortedValues1[6]);
+		
+		Integer[] values2 = {};
+		var sortedValues2 = sort.radixSort(values2);
+		
+		assertEquals(null, sortedValues2);
+		
+		
 	}
 
 }
