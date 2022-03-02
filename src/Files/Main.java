9b87d5225f342d7b03dd8e4543c2f2f9c20039ae
pabloozamora <pabloozamora02@gmail.com>
2022-03-01 23:13:30 -0600

    Estructura del proyecto corregida

--- src/Files/Main.java
+++ src/Files/Main.java
@@ -1,8 +1,9 @@
+package Files;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/Tests/SortTests.java
+++ src/Tests/SortTests.java
@@ -2,12 +2,17 @@ package Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import org.junit.jupiter.api.Test;
+
+import Files.CompareInt;
+import Files.Sort;
 
 class SortTests {
 
 	@Test
 	void test() {
+		Sort prueba = new Sort(new CompareInt());
 		fail("Not yet implemented");
 	}
 
